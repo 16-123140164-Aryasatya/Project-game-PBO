5629ae63fb96ea9b53022ee174405dabb5f0726b
16-123140164-Aryasatya <aryasatya.123140164@student.itera.ac.id>
2025-05-18 22:25:19 +0700

    Delete Project Game PBO/src directory

--- Project Game PBO/src/test.py
+++
@@ -1,8 +0,0 @@
-import pygame
-
-pygame.init()
-screen = pygame.display.set_mode((1, 1))  # Wajib supaya convert_alpha bisa jalan
-
-shield_spritesheet = pygame.image.load("assets/shield.png").convert_alpha()
-obstacle_img = pygame.image.load("assets/multiplier.png").convert_alpha()
-print("Ukuran shield.png:", obstacle_img.get_size())
